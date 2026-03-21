975de423c7d65755779cfc61d419da9cfa74164a
pngu <vanschmusnolan@gmail.com>
2026-03-20 17:03:08 -0700

    Devin Cleanup 1

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ export const R2_BASE_URL = "https://assets.pngu.info";
 export const R2_BRAND_BASE = `${R2_BASE_URL}/PNGU-Brand-Assets-main`;
 export const R2_DATA_BASE = `${R2_BASE_URL}/PNGU-Blaster-Data-main`;
 export const R2_FILES_BASE = `${R2_BASE_URL}/PNGU-3D-Files-main`;
+export const R2_HIGHLIGHT_BASE = `${R2_BASE_URL}/highlight`;
