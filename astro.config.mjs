aa993a8aeb86a95d7a49b8f75112896b7d7a77f9
Nolan Van Schmus <vanschmusnolan@gmail.com>
2026-05-06 18:17:13 -0700

    Enhance astro.config.mjs with build and vite settings
    Added build and vite configurations for improved performance and integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
@@ -6,7 +7,7 @@ import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   site: "https://pngu.info",
-  output: "hybrid", // Allows Keystatic to run as a Worker
+  output: "hybrid",
   integrations: [
     react(),
     markdoc(),
@@ -14,5 +15,21 @@ export default defineConfig({
   ],
   adapter: cloudflare({
     platformProxy: { enabled: true },
+    runtime: { mode: 'complete' }
   }),
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+  },
+  vite: {
+    // This tells Vite to replace these strings with the actual values 
+    // from your Cloudflare environment during the build process.
+    define: {
+      'process.env.KEYSTATIC_GITHUB_CLIENT_ID': JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_ID),
+      'process.env.KEYSTATIC_GITHUB_CLIENT_SECRET': JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_SECRET),
+    },
+    ssr: {
+      external: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner']
+    }
+  }
 });
